d2da574aaabca43dac9d841e23d0931cecfd1875
simrabhombal <141501770+simrabhombal@users.noreply.github.com>
2024-03-17 21:28:52 +0530

    fixed unused import

--- Disease/CoronaryDisease/urls.py
+++ Disease/CoronaryDisease/urls.py
@@ -2,7 +2,7 @@
 
 from django.urls import path
 from . import views 
-from .views import base, home, about, login, contact, result, predict_coronary_disease
+from .views import base, home, about, login, contact, result
 
 urlpatterns = [
     path('', views.base, name='base'),
@@ -11,6 +11,6 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('contact/', contact, name='contact'),
     path('result/', result, name='result'),
-    path('predict/', predict_coronary_disease, name='predict_coronary_disease'),
+   # path('predict/', predict_coronary_disease, name='predict_coronary_disease'),
 ]
 
